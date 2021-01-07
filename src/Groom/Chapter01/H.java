50d0981aa0a6dc64b05413f35124998af85774e6
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-01-07 23:57:17 +0900

    H

---
+++ src/Groom/Chapter01/H.java
@@ -0,0 +1,48 @@
+package Groom.Chapter01;
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+public class H {
+    public static final Scanner scanner = new Scanner(System.in);
+
+
+    /**
+     * 평균과의 차가 가장 작은 데이터의 번호를 반환하는 함수
+     * 여러 가지라면 가장 빠른 번호를 반환한다.
+     *
+     * @param data
+     * @param n
+     * @return int  가장 평균과 가까운 데이터의 번호 (1번부터 시작)
+     */
+    public static int findIndex(int[] data, int n)
+    {
+        int avr = 0;
+        int sum = 0;
+        for (int i = 0 ; i < n ; i++){
+            sum += data[i];
+        }
+        avr = sum/n;
+
+
+        return index;
+    }
+
+    public static void main(String[] args)
+    {
+        int n = scanner.nextInt();
+        int[] data = new int[n];
+        for(int i = 0 ; i < n ; i++)
+        {
+            data[i] = scanner.nextInt();
+        }
+
+        int answer = findIndex(data, n);
+        int index = answer - 1;
+
+        System.out.printf("%d %d\n", answer, data[index]);
+    }
+
+}
