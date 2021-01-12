47a7e2f9dbb8b151c58890012c101e2a3465a5dd
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-01-12 22:30:08 +0900

    H

--- src/Groom/Chapter01/J.java
+++ src/Groom/Chapter01/J.java
@@ -14,13 +14,23 @@ public class J {
      * @return
      */
     public static int getRangeSumFromOne(int i) {
-
+        int sum = 0;
+        for (int j = 1 ; j <= i ; j++){
+            for (int k = 1 ; k <= j ; k++){
+                sum += k;
+            }
+        }
+        return sum;
     }
 
 
     public static long getAnswer(int N)
     {
+        int getAnswer = 0;
 
+        getAnswer = getRangeSumFromOne(N);
+
+        return getAnswer;
     }
 
 
