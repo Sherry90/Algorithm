f91767fb6fa4278030695f2719ad584fc1065c9e
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-01-13 21:38:39 +0900

    b1000

---
+++ src/HumanOrAnimal/class01/b1000.java
@@ -0,0 +1,16 @@
+package HumanOrAnimal.class01;
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class b1000 {
+//public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a+b);
+        scanner.close();
+    }
+}
