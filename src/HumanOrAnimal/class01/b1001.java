cd0169ad670d077150c5998e900b50b3bc3096dc
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-01-13 22:02:03 +0900

    b1001, b1008

---
+++ src/HumanOrAnimal/class01/b1001.java
@@ -0,0 +1,16 @@
+package HumanOrAnimal.class01;
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class b1001 {
+    public static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a-b);
+        scanner.close();
+    }
+}
---
+++ src/HumanOrAnimal/class01/b1008.java
@@ -0,0 +1,17 @@
+package HumanOrAnimal.class01;
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class b1008 {
+    public static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double a = scanner.nextInt();
+        double b = scanner.nextInt();
+        double val = a/b;
+        System.out.println(val);
+        scanner.close();
+    }
+}
