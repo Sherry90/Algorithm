e3af5aaf5d6f3c8bca135a0955edde8145b3e54e
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-01-17 03:06:41 +0900

    b1157

--- src/HumanOrAnimal/class01/b1157.java
+++ src/HumanOrAnimal/class01/b1157.java
@@ -5,13 +5,27 @@ import java.lang.*;
 import java.util.*;
 
 public class b1157 {
-    public static final Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) {
-        String string = scanner.nextLine();
-        String[] stringArray;
-        stringArray = string.split("");
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next().toUpperCase();
 
+        int[] count = new int[26];
 
+        for (int i = 0; i < str.length(); i++) {
+            int num = str.charAt(i) -'A' ;
+            count[num]++;
+        }
+
+        int max = 0;
+        char answer = '?';
+        for (int i = 0; i < count.length; i++) {
+            if(max < count[i]){
+                max = count[i];
+                answer = (char)(i+'A');
+            } else if (max == count[i]){
+                answer = '?';
+            }
+        }
+        System.out.println(answer);
     }
 }
