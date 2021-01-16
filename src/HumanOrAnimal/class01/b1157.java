10081de2761797127784e3afc7aea20d3dd62d74
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-01-16 23:22:24 +0900

    b1157

---
+++ src/HumanOrAnimal/class01/b1157.java
@@ -0,0 +1,17 @@
+package HumanOrAnimal.class01;
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class b1157 {
+    public static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String string = scanner.nextLine();
+        String[] stringArray;
+        stringArray = string.split("");
+
+
+    }
+}
