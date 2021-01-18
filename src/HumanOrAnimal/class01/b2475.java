f230ef81678f87e6d11f2b3504aafdf305970305
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-01-18 23:10:15 +0900

    b2475

---
+++ src/HumanOrAnimal/class01/b2475.java
@@ -0,0 +1,20 @@
+package HumanOrAnimal.class01;
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class b2475 {
+    public static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        String string = String.valueOf(n);
+
+        String[] stringArray = new String[6];
+        for (int i = 0 ; i < 6 ; i++){
+            //stringArray[i] = string[i];
+        }
+
+    }
+}
