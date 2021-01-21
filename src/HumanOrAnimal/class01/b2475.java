93d926bb8ffb6d693bb807a95d6d6ace3b0ccec9
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-01-21 22:11:45 +0900

    b2475

--- src/HumanOrAnimal/class01/b2475.java
+++ src/HumanOrAnimal/class01/b2475.java
@@ -3,26 +3,15 @@ package HumanOrAnimal.class01;
 import java.io.*;
 import java.lang.*;
 import java.util.*;
-import java.util.stream.Stream;
 
 public class b2475 {
     public static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        //int n = scanner.nextInt();
-        //String string = String.valueOf(n);
-
-        String[] stringArray = new String[6];
-        for (int i = 0 ; i < 6 ; i++){
-            //stringArray[i] = string[i];
+        int cnt = 0;
+        for (int i = 0; i < 5; i++) {
+            cnt += Math.pow(scanner.nextInt(),2);
         }
-        int number = 12345;
-        int[] digits = Stream.of(String.valueOf(number).split("")).mapToInt(Integer::parseInt).toArray();
-        //System.out.print( Arrays.toString(digits) );
-
-        for (int i = 0 ; i < digits.length ; i++){
-            System.out.println(digits[i]);
-        }
-
+        System.out.println(cnt % 10);
     }
 }
