a8f28989da441e39c2a33679efe851d8afb94706
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-01-23 01:00:50 +0900

    99

---
+++ src/HumanOrAnimal/class01/b2739.java
@@ -0,0 +1,18 @@
+package HumanOrAnimal.class01;
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class b2739 {
+    public static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+
+        for (int i = 1 ; i <= 9 ; i ++){
+            System.out.println(n+" * "+i+" = "+n*i);
+
+        }
+    }
+}
