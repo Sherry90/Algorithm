3e74d666e71ff01707e12b3aa33b5d7fd62849ec
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-01-23 01:10:17 +0900

    b2742

---
+++ src/HumanOrAnimal/class01/b2742.java
@@ -0,0 +1,18 @@
+package HumanOrAnimal.class01;
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class b2742 {
+    public static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+
+        for (int i = n ; i > 0 ; i --){
+            System.out.println(i);
+
+        }
+    }
+}
