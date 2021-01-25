4168cdd46a519b8e93efbdffe4d9be58b6b17a9c
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-01-25 23:04:38 +0900

    b10171

---
+++ src/HumanOrAnimal/class01/b10171.java
@@ -0,0 +1,16 @@
+package HumanOrAnimal.class01;
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class b10171 {
+    public static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
