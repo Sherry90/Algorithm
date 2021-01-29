656cc33693ab0b63de6fa53387b13d9fe2caaa4b
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-01-29 23:56:27 +0900

    b11654

---
+++ src/HumanOrAnimal/class01/b11654.java
@@ -0,0 +1,14 @@
+package HumanOrAnimal.class01;
+
+import java.util.Scanner;
+
+public class b11654 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int ch = scanner.next().charAt(0);
+
+        System.out.print(ch);
+    }
+}
