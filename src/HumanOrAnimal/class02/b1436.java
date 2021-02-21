2b00b450d92464aa049d3a782440848e9ed598b4
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-02-21 19:13:25 +0900

    b1436

---
+++ src/HumanOrAnimal/class02/b1436.java
@@ -0,0 +1,18 @@
+package HumanOrAnimal.class02;
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class b1436 {
+    public static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        String num = String.valueOf(n-1);
+        if(n == 1) num = "";
+        String s6 = "666";
+
+        System.out.println(num+s6);
+    }
+}
