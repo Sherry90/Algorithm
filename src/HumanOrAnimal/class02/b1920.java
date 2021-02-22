4ca27c9919b0c28bd6fac6e26c909b397d02da44
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-02-22 22:42:25 +0900

    b1920

---
+++ src/HumanOrAnimal/class02/b1920.java
@@ -0,0 +1,24 @@
+package HumanOrAnimal.class02;
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class b1920 {
+    public static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        int[] arrayN = new int[n];
+        for (int i = 0; i < arrayN.length; i++) {
+            arrayN[i] = scanner.nextInt();
+        }
+
+        int m = scanner.nextInt();
+        int[] arrayM = new int[m];
+        for (int i = 0; i < arrayM.length; i++) {
+            arrayM[i] = scanner.nextInt();
+        }
+
+    }
+}
