cb3ff5ef243f713f6aef80d41e4adf3724e110e9
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-02-28 18:37:46 +0900

    b1920 BinarySearch

--- src/HumanOrAnimal/class02/b1920.java
+++ src/HumanOrAnimal/class02/b1920.java
@@ -5,40 +5,33 @@ import java.lang.*;
 import java.util.*;
 
 public class b1920 {
+    public int solution(int[] arr, int num) {
+        int high = arr.length - 1, low = 0, mid = 0;
 
-    public static void main(String[] args) throws IOException {
-        // 이분탐색이 뭔데 씨발
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
-        st = new StringTokenizer(br.readLine()," ");
+        while (low <= high) {
+            mid = (high + low) / 2;
 
-        int n = Integer.parseInt(br.readLine());
-        int[] arrayN = new int[n];
-        for (int i = 0; i < arrayN.length; i++) {
-            arrayN[i] = Integer.parseInt(st.nextToken());
+            if (arr[mid] == num)
+                return 1;
+            else if (arr[mid] > num)
+                high = mid - 1;
+            else
+                low = mid + 1;
         }
 
-        int m = Integer.parseInt(br.readLine());
-        int[] arrayM = new int[m];
-        for (int i = 0; i < arrayM.length; i++) {
-            arrayM[i] = Integer.parseInt(st.nextToken());
-        }
+        return 0;
+    }
 
-        int count = 0;
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[] arr = new int[scan.nextInt()];
 
-        for (int i = 0; i < arrayM.length; i++) {
-            count = 0;
-            for (int j = 0; j < arrayM.length; j++) {
-                if(arrayM[i] == arrayN[j]){
-                    count++;
-                }
-            }
-            if(count > 0){
-                System.out.println(1);
-            }
-            else System.out.println(0);
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = scan.nextInt();
+        Arrays.sort(arr);
 
-        }
-
+        int m = scan.nextInt();
+        for (int i = 0; i < m; i++)
+            System.out.println(new b1920().solution(arr, scan.nextInt()));
     }
 }
