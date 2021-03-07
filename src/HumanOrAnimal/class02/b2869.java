5babcea4331768d5683f58b1c2442996f49de2c5
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-03-07 19:39:27 +0900

    b2869

--- src/HumanOrAnimal/class02/b2869.java
+++ src/HumanOrAnimal/class02/b2869.java
@@ -1,4 +1,32 @@
 package HumanOrAnimal.class02;
 
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
 public class b2869 {
+    public static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException{
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int V = scanner.nextInt();
+
+        int l = 0;
+        int day = 0;
+
+
+        while (l < V){
+            day++;
+            l += A;
+            if(l >= V){
+                break;
+            }
+            l -= B;
+        }
+
+
+        System.out.println(day);
+    }
 }
