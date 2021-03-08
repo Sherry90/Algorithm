d483c81476b143043c86e21ee1142cb98a2fd3e2
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-03-08 22:37:09 +0900

    B

---
+++ src/Groom/Chapter01/B.java
@@ -0,0 +1,40 @@
+package Groom.Chapter01;
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class B {
+    public static final Scanner scanner = new Scanner(System.in);
+
+    /**
+     * 배열의 최대값을 계산하는 함수.
+     *
+     * @param data
+     * @param n
+     * @return data[0] ~ data[n-1]중 최대값.
+     */
+    public static int getMax(int[] data, int n) {
+        int max = data[0];
+        for (int i = 0; i < data.length; i++) {
+            if(max < data[i]){
+                max = data[i];
+            }
+
+        }
+        return max;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int n = scanner.nextInt();
+        int[] data = new int[n];
+        for (int i = 0; i < n; i++) {
+            data[i] = scanner.nextInt();
+        }
+
+        int answer = getMax(data, n);
+
+        System.out.println(answer);
+    }
+
+}
