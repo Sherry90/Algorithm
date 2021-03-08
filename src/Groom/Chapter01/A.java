0e7f25438419716d03ec185a70b1791fed4defc2
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-03-08 22:32:04 +0900

    A

--- src/Groom/Chapter01/A.java
+++ src/Groom/Chapter01/A.java
@@ -15,11 +15,12 @@ public class A {
      * @return a와 b중 더 큰 값
      */
     public static int getMax(int a, int b) {
-        if (a>b) {
+        if(a > b){
             return a;
         } else {
             return b;
         }
+
     }
 
     public static void main(String[] args) throws Exception {
