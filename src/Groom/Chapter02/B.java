5fc595d1ec06f16e8e9a2d684be991c4be2607d1
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-03-13 19:44:14 +0900

    B

---
+++ src/Groom/Chapter02/B.java
@@ -0,0 +1,51 @@
+package Groom.Chapter02;
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class B {
+    public static final Scanner scanner = new Scanner(System.in);
+
+    /**
+     * 주어진 배열이 오름차순인지 검사하는 함수
+     * @param data
+     * @param n     데이터의 수
+     * @return      data[0] ~ data[n-1]이 오름차순이라면 true, else false
+     */
+    public static boolean isOrdered(int[] data, int n)
+    {
+        boolean tf = true;
+
+        for (int i = 1; i < data.length; i++) {
+            if(data[i-1] > data[i]){
+                tf = false;
+                break;
+            }
+
+        }
+
+        return tf;
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        int n = scanner.nextInt();
+        int[] data = new int[n];
+        for(int i = 0 ; i < n ; i++)
+        {
+            data[i] = scanner.nextInt();
+        }
+
+        boolean result = isOrdered(data, n);
+
+        if(result)
+        {
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+
+    }
+
+}
