5ac39652b0d830b5fd53f643f810654183381f76
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-03-20 21:48:28 +0900

    J

---
+++ src/Groom/Chapter02/J.java
@@ -0,0 +1,53 @@
+package Groom.Chapter02;
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+public class J {
+    public static final Scanner scanner = new Scanner(System.in);
+
+    /**
+     * 게임의 규칙에 따라 현무가 승리할 수 있는 경우의 수가 존재하는지 검사하는 함수
+     *
+     * @param data
+     * @param n
+     * @param k
+     * @return true   현무가 승리할 수 있는 경우의 수가 하나 이상 존재한다면
+     * @return false  else
+     * 인접한 값의 합이 짝수가 될 확률이 있으면 YES 아니면 NO
+     */
+    public static boolean isWinnable(int[] data, int n, int k) {
+
+        int winCount = 0;
+        long sum = 0;
+
+        if(winCount > 0)
+        {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        int[] data = new int[n];
+        for(int i = 0 ; i < n ; i++)
+        {
+            data[i] = scanner.nextInt();
+        }
+
+        boolean result = isWinnable(data, n, k);
+
+        if(result)
+        {
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+    }
+
+}
