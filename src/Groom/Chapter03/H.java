ef19e6444d00bea7916b32726b9e2b54f40702ed
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-04-10 20:39:41 +0900

    F

--- src/Groom/Chapter03/H.java
+++ src/Groom/Chapter03/H.java
@@ -8,18 +8,51 @@ public class H {
     public static final Scanner scanner = new Scanner(System.in);
 
     /**
-     * 중복을 포함해 두 카드의 합으로 만들 수 있는 당첨번호의 수를 계산하는 함수
+     * 중복을 포함해 세 카드의 합으로 만들 수 있는 당첨번호들의 리스트를 반환하는 함수
      * @param n     카드의 수
      * @param m     검사하려는 당첨번호의 수
      * @param cards   각 카드에 적힌 숫자들
      * @param target  검사하려는 각 당첨번호 리스트
-     * @return
+     * @return      세 카드의 합으로 표현될 수 있는 당첨번호들의 오름차순 리스트
      */
-    public static int getPossibleTargetNumber(int n, int m, int[] cards, int[] target)
+    public static ArrayList<Integer> getPossibleTargets(int n, int m, int[] cards, int[] target)
     {
-        int answer = 0; //만들 수 있는 당첨번호의 수
+        ArrayList<Integer> possibleTargets = new ArrayList<>(); //만들 수 있는 당첨번호들
 
-        return answer;
+        Arrays.sort(cards);
+
+        for(int k : target)
+        { //검사 할 모든 당첨 번호 k에 대하여
+
+            boolean possible = false; // k를 세 숫자의 합으로 표현할 수 있는지 여부
+            for(int i = 0 ; i < n ; i+= 1)
+            {   // 카드 중 하나 x를 선택한다.
+                int x = cards[i];
+                for(int j = 0 ; j <= i ; j += 1)
+                {   // 카드 중 하나 y를 선택한다
+                    int y = cards[j];
+                    int z = k - (x + y); // k = (x +  y )+ z 가 되는 z를 계산한다
+
+                    // z가 cards[]에 존재한다면?
+                    if( Arrays.binarySearch(cards, z) >= 0 ) {
+                        // k = x + y + z 가 가능한 <x, y, z>가 존재한다.
+                        possible = true;
+                        break;
+                    }
+                }
+                if(possible)
+                {   // 이미 찾았다면 탈출한다
+                    break;
+                }
+            }
+            if(possible)
+            {	// 세 카드의 합으로 k를 만들 수 있다면, 추가한다.
+                possibleTargets.add(k);
+            }
+        }
+
+        Collections.sort(possibleTargets);
+        return possibleTargets;
     }
 
     public static void main(String[] args) throws Exception {
@@ -41,9 +74,21 @@ public class H {
             targets[i] = scanner.nextInt();
         }
 
-        int answer = getPossibleTargetNumber(n, m, cards, targets);
+        ArrayList<Integer> answers = getPossibleTargets(n, m, cards, targets);
 
-        System.out.println(answer);
+        if(answers.size() == 0)
+        {
+            System.out.print("NO");
+        }else{
+            for(int i = 0 ; i < answers.size() ; i++)
+            {
+                if( i > 0 )
+                {
+                    System.out.print(" ");
+                }
+                System.out.print(answers.get(i));
+            }
+        }
     }
 
 }
