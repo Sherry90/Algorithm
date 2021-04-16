95287da7bf46bdc709db288f44f723cca8513915
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-04-16 21:37:18 +0900

    H

--- src/Groom/Chapter03/I.java
+++ src/Groom/Chapter03/I.java
@@ -1,5 +1,7 @@
 package Groom.Chapter03;
 
+import org.w3c.dom.Attr;
+
 import java.io.*;
 import java.lang.*;
 import java.util.*;
@@ -19,6 +21,39 @@ public class I {
     {
         ArrayList<Integer> possibleTargets = new ArrayList<>(); //만들 수 있는 당첨번호들
 
+        Arrays.sort(cards);
+
+        for(int k : target)
+        { //검사 할 모든 당첨 번호 k에 대하여
+
+            boolean possible = false; // k를 세 숫자의 합으로 표현할 수 있는지 여부
+            for(int i = 0 ; i < n ; i+= 1)
+            {   // 카드 중 하나 x를 선택한다.
+                int x = cards[i];
+                for(int j = 0 ; j <= i ; j += 1)
+                {   // 카드 중 하나 y를 선택한다
+                    int y = cards[j];
+                    int z = k - (x + y); // k = (x +  y )+ z 가 되는 z를 계산한다
+
+                    // z가 cards[]에 존재한다면?
+                    if( Arrays.binarySearch(cards, z) >= 0 ) {
+                        // k = x + y + z 가 가능한 <x, y, z>가 존재한다.
+                        possible = true;
+                        break;
+                    }
+                }
+                if(possible)
+                {   // 이미 찾았다면 탈출한다
+                    break;
+                }
+            }
+            if(possible)
+            {	// 세 카드의 합으로 k를 만들 수 있다면, 추가한다.
+                possibleTargets.add(k);
+            }
+        }
+
+        Collections.sort(possibleTargets);
         return possibleTargets;
     }
 
