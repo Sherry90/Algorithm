0bfd60ab4073a4a44d6bafc69da98a5763037177
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-07-18 20:43:33 +0900

    10998 a*b

---
+++ src/baekjoon/b10998.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class b10998 {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(a * b);
+    }
+}
