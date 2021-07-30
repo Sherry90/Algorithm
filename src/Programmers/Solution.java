da4528d46be3fadf3bdd2d38cc502ae44f60e43e
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-07-30 20:29:36 +0900

    숫자 문자열과 영단어

--- src/Programmers/Solution.java
+++ src/Programmers/Solution.java
@@ -1,35 +1,29 @@
 package Programmers;
 
 class Solution {
-    public static long solution(int a, int b) {
-        long answer = 0;
+    public static int solution(String s) {
+        int answer = 0;
 
-        int min = 0;
-        int max = 0;
+        s = s.replace("zero","0");
+        s = s.replace("one","1");
+        s = s.replace("two","2");
+        s = s.replace("three","3");
+        s = s.replace("four","4");
+        s = s.replace("five","5");
+        s = s.replace("six","6");
+        s = s.replace("seven","7");
+        s = s.replace("eight","8");
+        s = s.replace("nine","9");
 
-        if(a == b){
-            answer = (long)a;
-        } else {
-            if(a > b){
-                max = a;
-                min = b;
-            } else if (a < b){
-                max = b;
-                min = a;
-            }
-
-            for (int i = min; i <= max; i++) {
-                answer += i;
-            }
-        }
+        answer = Integer.parseInt(s);
 
         return answer;
     }
 
     public static void main(String[] args) {
-        int a = 5;
-        int b = 3;
-        long result = solution(a, b);
-        System.out.println(result);
+        String s = "one4seveneight";
+        int number = solution(s);
+
+        System.out.println(number);
     }
 }
