52a519da5da90074e4b0525f3ff4d49fdf87fd40
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-08-02 17:28:09 +0900

    부족한 금액 계산하기

--- src/Programmers/Solution.java
+++ src/Programmers/Solution.java
@@ -1,28 +1,19 @@
 package Programmers;
 
-import java.util.Arrays;
-
 class Solution {
-    public int solution(int[] nums) {
-        int answer = 0;
-        int chooseChance = 0;
-        int different = 1;
+    public long solution(int price, int money, int count) {
+        long answer = 0;
+        long totalPrice = 0;
 
-        chooseChance = nums.length / 2;
-
-        Arrays.sort(nums);
-
-        for (int i = 1; i < nums.length; i++) {
-            if(nums[i-1] != nums[i]){
-                different++;
-            }
-        }
-        if (chooseChance > different) {
-            answer = different;
-        } else {
-            answer = chooseChance;
+        for (int i = 1 ; i <= count ; i ++){
+            totalPrice += (long)price * (long)i;
         }
 
+        answer = totalPrice - money;
+
+        if(money >= totalPrice){
+            answer = 0;
+        }
 
         return answer;
     }
