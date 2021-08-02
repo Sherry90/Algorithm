8e71ab26251576b8b4950130d88db9b68d8d0bcc
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-08-02 16:28:50 +0900

    폰켓몬

--- src/Programmers/Solution.java
+++ src/Programmers/Solution.java
@@ -1,10 +1,29 @@
 package Programmers;
 
-class Solution {
-    public int solution(String s) {
-        int answer = 0;
+import java.util.Arrays;
 
-        answer = Integer.parseInt(s);
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        int chooseChance = 0;
+        int different = 1;
+
+        chooseChance = nums.length / 2;
+
+        Arrays.sort(nums);
+
+        for (int i = 1; i < nums.length; i++) {
+            if(nums[i-1] != nums[i]){
+                different++;
+            }
+        }
+        if (chooseChance > different) {
+            answer = different;
+        } else {
+            answer = chooseChance;
+        }
+
+
         return answer;
     }
 }
