18e753b28d5bbe26c122a3283bbf6cb52557e176
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-08-22 19:49:09 +0900

    Chapter01 finish

---
+++ src/Groom/Chapter01/I.java
@@ -0,0 +1,48 @@
+package Groom.Chapter01;
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class I {
+    public static final Scanner scanner = new Scanner(System.in);
+
+    public static void selectionSort(int[] data, int n)
+    {
+        for(int i = 0 ; i < n ; i++)
+        {
+            int index = i;
+            for (int j = i ; j < n ; j++)
+            {
+                if (data[index] > data[j]){
+                    index = j;
+                }
+            }
+
+            int tmp = data[index];
+            data[index] = data[i];
+            data[i] = tmp;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int n = scanner.nextInt();
+        int[] data = new int[n];
+        for(int i = 0 ; i < n ; i++)
+        {
+            data[i] = scanner.nextInt();
+        }
+
+        selectionSort(data, n);
+
+        for(int i = 0; i < n ; i ++)
+        {
+            if( i > 0 )
+            {
+                System.out.print(" ");
+            }
+            System.out.print(data[i]);
+        }
+    }
+
+}
