ba6754b33ef6b4093b9ed2484f73605e669d798c
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-09-25 20:46:47 +0900

    n % 42 count

---
+++ src/baekjoon/step5/b3052.java
@@ -0,0 +1,35 @@
+package baekjoon.step5;
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class b3052 {
+    public static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int data[] = new int[10];
+
+        int calculated[] = new int[10];
+
+        int count = 1;
+
+        for (int i = 0; i < data.length; i++) {
+            data[i] = scanner.nextInt();
+        }
+
+        for (int i = 0; i < data.length; i++) {
+            calculated[i] = data[i] % 42;
+        }
+        Arrays.sort(calculated);
+
+        for (int i = 1; i < data.length; i++) {
+            if(calculated[i-1] != calculated[i]){
+                count++;
+            }
+        }
+        System.out.println(count);
+
+    }
+}
