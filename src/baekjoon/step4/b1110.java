931055e7dfc928d49579b7415695a1ee8fd7e51b
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-09-25 17:05:58 +0900

    while

---
+++ src/baekjoon/step4/b1110.java
@@ -0,0 +1,35 @@
+package baekjoon.step4;
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class b1110 {
+    public static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.close();
+        int cycles = 0;
+
+        int value = 0; // 몫
+        int remainder = 0; // 나머지
+
+        int number= N; // 새로운 수
+
+        while (true) {
+            value = number / 10;
+            remainder = number % 10;
+
+            number = remainder*10 + (value+remainder)%10;
+            cycles++;
+
+            if(N == number){
+                System.out.println(cycles);
+                break;
+            }
+
+        }
+
+    }
+}
