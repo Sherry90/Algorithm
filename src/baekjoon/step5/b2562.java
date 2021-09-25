ce501584aaaae011c509f76d1c25b01e5290359f
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-09-25 18:15:35 +0900

    array

---
+++ src/baekjoon/step5/b2562.java
@@ -0,0 +1,31 @@
+package baekjoon.step5;
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class b2562 {
+    public static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int[] data = new int[9];
+
+        for (int i = 0; i < data.length; i++) {
+            data[i] = scanner.nextInt();
+        }
+
+        int Max = data[0];
+        int index = 0;
+
+        for (int i = 0; i < data.length; i++) {
+            if(data[i] > Max){
+                Max = data[i];
+                index = i;
+            }
+        }
+        System.out.println(Max);
+        System.out.println(index+1);
+
+    }
+}
