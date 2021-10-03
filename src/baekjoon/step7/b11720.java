80b603cb4e8d487bdef3cd68e311fb456440d3d5
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-10-03 18:16:34 +0900

    숫자의 합

---
+++ src/baekjoon/step7/b11720.java
@@ -0,0 +1,23 @@
+package baekjoon.step7;
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class b11720 {
+    public static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int N = scanner.nextInt();
+        String str = scanner.next();
+        scanner.close();
+
+        int sum = 0;
+
+        for(int i = 0; i < N; i++) {
+            sum += str.charAt(i)-'0';
+        }
+        System.out.print(sum);
+    }
+}
