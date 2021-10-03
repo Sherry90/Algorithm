1f5292693fd6931f6b35ddec3d444e3e56228346
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-10-03 17:11:24 +0900

    아스키

---
+++ src/baekjoon/step7/b11654.java
@@ -0,0 +1,17 @@
+package baekjoon.step7;
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class b11654 {
+    public static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        char ch = scanner.nextLine().charAt(0);
+
+        int number = (int)ch;
+
+        System.out.println(number);
+    }
+}
