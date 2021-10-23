3a90e0b33730e84a92b1c4de79c5eaf38d6a2bee
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2021-10-23 18:23:27 +0900

    팩토리얼

--- src/baekjoon/setp10_재귀/b10872.java
+++ src/baekjoon/setp10_재귀/b10872.java
@@ -1,4 +1,20 @@
 package baekjoon.setp10_재귀;
 
+import java.lang.*;
+import java.util.*;
+
 public class b10872 {
+    public static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+
+        long Number = 1;
+
+        for (int i = 1; i < N; i++) {
+            Number += Number * i;
+        }
+
+        System.out.println(Number);
+    }
 }
