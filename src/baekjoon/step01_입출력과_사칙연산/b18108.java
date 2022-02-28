82cbfa5c2b197fea6569b20bcd9889b7b6a1cf75
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2022-03-01 00:18:42 +0900

    b18108

---
+++ src/baekjoon/step01_입출력과_사칙연산/b18108.java
@@ -0,0 +1,12 @@
+package baekjoon.step01_입출력과_사칙연산;
+
+import java.util.Scanner;
+
+public class b18108 {
+    public static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int year = scanner.nextInt();
+        System.out.println(year-543);
+    }
+}
