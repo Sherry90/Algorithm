567a55b209c2ea1a3bf25bc96a479799ce6a682e
Sherry90 <67566081+Sherry90@users.noreply.github.com>
2022-03-01 00:13:19 +0900

    b10926

---
+++ src/baekjoon/step01_입출력과_사칙연산/b10926.java
@@ -0,0 +1,12 @@
+package baekjoon.step01_입출력과_사칙연산;
+
+import java.util.Scanner;
+
+public class b10926 {
+    public static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String str = scanner.nextLine();
+        System.out.println(str+"??!");
+    }
+}
